3b838a9f6657a8170dd170a77b219899a3121b98
arif <arif@arifs.localdomain>
2008-04-26 00:47:01 +0500

    Refactored main.js by using classes in javascript

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,25 @@
-toggle_image = function() {
-  obj_name = "#" + this.id + " img";
-  obj = $(obj_name);
-  current_src = obj.attr("src");
-  if(current_src.search("_sel") == -1) {
-    new_src = current_src.replace(/.png$/, "_sel.png");
-    obj.attr("src", new_src);
+var MainClass = function(){};
+
+MainClass.prototype = {
+  initialize : function() {
+    $("#navigation-header a").hover(main_class.toggle_image, main_class.toggle_image);
+  },
+  toggle_image : function() {
+    obj_name = "#" + this.id + " img";
+    obj = $(obj_name);
+    current_src = obj.attr("src");
+    if(current_src.search("_sel") == -1) {
+      new_src = current_src.replace(/.png$/, "_sel.png");
+      obj.attr("src", new_src);
+    }
+    else {
+      new_src = current_src.replace(/_sel.png$/, ".png");
+      obj.attr("src", new_src);
+    }
   }
-  else {
-    new_src = current_src.replace(/_sel.png$/, ".png");
-    obj.attr("src", new_src);
-  }
-}
+};
 
 $(document).ready(function(event){
-  $("#navigation-header a").hover(toggle_image, toggle_image);
+  main_class = new MainClass;
+  main_class.initialize();
 });
