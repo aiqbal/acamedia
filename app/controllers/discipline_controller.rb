53db77c94a2863589ed05e480bf2c9f36f4a4f00
arif <arif@arifs.localdomain>
2008-04-27 12:05:31 +0500

    Discipline: Added support for creating and viewing discipline

--- app/controllers/discipline_controller.rb
+++ app/controllers/discipline_controller.rb
@@ -1,9 +1,18 @@
 class DisciplineController < ApplicationController
-
+  layout 'main'
   def create
+    if request.post?
+      @discipline = Discipline.new(params[:discipline])
+      @discipline.creator = get_session_user
+      if @discipline.save
+        flash[:notice] = get_message('created_successfully')
+        redirect_to :action => 'view', :id => @discipline.name
+      end
+    end
   end
 
   def view
+    @discipline = Discipline.find_by_name(params[:id])
   end
 
   def update
