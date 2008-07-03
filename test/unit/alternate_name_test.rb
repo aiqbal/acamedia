070ac24c2cd708b3d13b56f8b31e56e956c1a27c
Muhammad Zeeshan Abid <zeeshan@zeeshan-desktop.(none)>
2008-07-04 02:17:18 +0600

    AlternateNameTest: method get_original_object test case added

--- test/unit/alternate_name_test.rb
+++ test/unit/alternate_name_test.rb
@@ -4,5 +4,7 @@ class AlternateNameTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   def test_truth
     assert_equal(@user1,@alternate_name1.user) 
+    assert_equal(@course1,@alternate_name1.get_original_object())
+    assert_equal(@course2,@alternate_name3.get_original_object())
   end
 end
